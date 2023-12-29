d9a1f1dab344ec7b1399b862648c612dc4bf662b
amishana1k <132745732+amishana1k@users.noreply.github.com>
2023-12-29 23:44:40 +0530

    Findthedifference.py
    You are given two strings s and t.
    
    String t is generated by random shuffling string s and then add one more letter at a random position.
    
    Return the letter that was added to t.

---
+++ Findthedifference.py
@@ -0,0 +1,7 @@
+#Find the Difference
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for x in t:
+            if x not in s:
+                return x 
+            s=s.replace(x,"",1)
